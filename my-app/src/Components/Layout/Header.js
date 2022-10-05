bce0881ddbe41269a4fc61525b6751340f0cd4c8
webrasa <zivanovicratibor@gmail.com>
2022-10-05 11:40:48 +0200

    Header

--- my-app/src/Components/Layout/Header.js
+++ my-app/src/Components/Layout/Header.js
@@ -3,19 +3,22 @@ import classes from './Header.module.css';
 
 import SearchIcon from '../../Assets/svg/search.svg'
 
-
-
 const Header = props => {
 
 return <Fragment>
 
 <header className = {classes.header}>
-    <h1>React</h1>
-    <h2>React2</h2>
+     <nav class="header__nav" id="navbar">
+        <ul class="header__list">
+          <li class="header__item"><a href="/" class="header__link">HOME</a></li>
+          <li class="header__item"><a href="/" class="header__link">ABOUT US</a></li>
+          <li class="header__item"><a href="/" class="header__link">OUR WORK</a></li>
+          <li class="header__item"><a href="/" class="header__link">CONTACT</a></li>
+        </ul>
+      </nav>
+
 </header>
-<div className = {classes['main-image']}>
-  <img src={SearchIcon} alt="Search icon"/>
-</div>
+
 
 </Fragment>;
 
