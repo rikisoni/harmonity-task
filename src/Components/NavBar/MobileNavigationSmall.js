5f4d0d77f053344b52f24b75cb81550b038bcdb4
webrasa <zivanovicratibor@gmail.com>
2022-10-16 01:04:57 +0200

    Changed slider

--- src/Components/NavBar/MobileNavigationSmall.js
+++ src/Components/NavBar/MobileNavigationSmall.js
@@ -1,18 +1,22 @@
 import NavLinks from "./NavLinks";
 import classes from './NavBar.module.css';
-import Burger from '../../Assets/svg/burger.svg'
+// import Burger from '../../Assets/svg/burger.svg'
 import { useState } from "react";
 
 const MobileNavigationSmall = () => {
-    
+
     const [open, setOpen] = useState(false);
 
     return (
         <nav className={classes.MobileNavigationSmall}>
-            <div className={classes.BurgerMenu} onClick={()=>setOpen(!open)}>
-                <img src={Burger}></img>
+            <div className={classes.BurgerMenu} onClick={() => setOpen(!open)} >
+                <svg xmlns="true" viewBox="0 0 52.3 41.3" fill="grey"> 
+                    <path class="st0" d="M52.3,4.7c0,2.6-2.3,4.7-5.2,4.7H5.2C2.3,9.5,0,7.3,0,4.7l0,0C0,2.1,2.3,0,5.2,0h41.9C50,0,52.3,2.1,52.3,4.7L52.3,4.7z"/>
+                    <path class="st0" d="M52.3,20.7c0,2.6-2.3,4.7-5.2,4.7H5.2c-2.9,0-5.2-2.1-5.2-4.7l0,0c0-2.6,2.3-4.7,5.2-4.7h41.9C50,15.9,52.3,18,52.3,20.7L52.3,20.7z"/>
+                    <path class="st0" d="M52.3,36.6c0,2.6-2.3,4.7-5.2,4.7H5.2c-2.9,0-5.2-2.1-5.2-4.7l0,0c0-2.6,2.3-4.7,5.2-4.7h41.9C50,31.8,52.3,34,52.3,36.6L52.3,36.6z"/>
+                </svg>
             </div>
-           {open && <NavLinks />} 
+            {open && <NavLinks />}
         </nav>
 
     );
